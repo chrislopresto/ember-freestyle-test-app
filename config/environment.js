f69307dafab00bb15e03d0928416b31217f69e53
Chris LoPresto <chris@chrislopresto.com>
2017-05-03 03:15:01 -0400

    Install Ember Freestyle (#1)
    * Add hello-world component
    * Install ember-freestyle
    * Configure ember-cli-deploy

--- config/environment.js
+++ config/environment.js
@@ -43,7 +43,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.rootURL = '/ember-freestyle-test-app';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
